6dd4db4870394f812cc9dc808dc9082a8d6821f1
devlopersabbir <devlopersabbir@gmail.com>
2023-08-24 15:36:30 +0600

    Navbar and Footer init

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,6 @@
-export {default as Hero} from './Hero'
-export {default as CustomButton} from './custom/CustomButton'
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+
+// custom
+export { default as CustomButton } from "./custom/CustomButton";
