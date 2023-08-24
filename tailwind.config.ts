56dce74241f67679babf93601f0a0dd3d473d445
devlopersabbir <devlopersabbir@gmail.com>
2023-08-24 14:02:12 +0600

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  mode: "jit",
   theme: {
     extend: {
       fontFamily: {
@@ -31,4 +31,7 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+export default config
+
+
